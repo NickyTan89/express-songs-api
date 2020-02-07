2004323325e679a174b5cbe8e9136e7be98bb00b
Nicholas <nicholas@massiveinfinity.com>
2020-02-07 14:41:50 +0800

    replace demo code with mine.

--- tests/songs.test.js
+++ tests/songs.test.js
@@ -2,77 +2,82 @@ const app = require("../app");
 const request = require("supertest");
 
 describe("routes/songs", () => {
-
+  
   it("POST /songs should return a new song object", () => {
-    const requestBody = {name: "test song", artist: "rhianna"};
-    const responseBody = {id: 1, name: "test song", artist: "rhianna"};
+    const requestBody = { name: "Numb", artist: "Linkin Park" };
+    const responseBody = { id: 1, name: "Numb", artist: "Linkin Park" };
+
     return request(app)
     .post("/songs")
     .send(requestBody)
-    
+
     .then(response => {
       expect(response.status).toEqual(201);
       expect(response.body).toEqual(responseBody)
     });
   });
   
-  it("GET /songs should return an array containing one song", () => {
+  it("GET /songs should return an array with a song", () => {
+    const responseBody = [
+      { id:1, name: "Numb", artist: "Linkin Park" }
+    ];
+
     return request(app)
     .get("/songs")
-    
+
     .then(response => {
       expect(response.status).toEqual(200);
-      expect(Array.isArray(response.body)).toEqual(true);
-      expect(response.body.length).toEqual(1);
+      expect(response.body).toEqual(responseBody)
+    });
+  });
+
+  it("GET /songs/:id should return song with id specified", () => {
+    const responseBody = { id: 1, name: "Numb", artist: "Linkin Park" }
+
+    return request(app)
+    .get('/songs/1')
+
+    .then(response => {
+      expect(response.status).toEqual(200);
+      expect(response.body).toEqual(responseBody)
     });
   });
   
-  it("GET /songs/:id should return the song with id", () => {
-    const responseBody = {id: 1, name: "test song", artist: "rhianna"};
-    
+  it("PUT /songs/:id should return the updated song", () => {
+    const requestBody = { name: "Dance Monkey", artist: "Tones And I" }
+    const responseBody = { id: 1, name: "Dance Monkey", artist: "Tones And I" }
+
     return request(app)
-    .get("/songs/1")
-    
-    .then(response => {
-      expect(response.status).toEqual(200);
-      expect(response.body).toMatchObject(responseBody);
-    });
-  });
-    
-  it("PUT /songs should return the updated song", () => {
-    const requestBody = {name: "updated song", artist: "rhianna"};
-    const responseBody = {id: 1, name: "updated song", artist: "rhianna"};
-    
-    return request(app)
-    .put("/songs/1")
+    .put('/songs/1')
     .send(requestBody)
-    
+
     .then(response => {
       expect(response.status).toEqual(200);
-      expect(response.body).toEqual(responseBody);
+      expect(response.body).toEqual(responseBody)
     });
   });
 
   it("DELETE /songs/:id should return the deleted song", () => {
-    responseBody = { id: 1, name: "updated song", artist: "rhianna" };
+    const responseBody = { id: 1, name: "Dance Monkey", artist: "Tones And I" }
 
     return request(app)
-    .delete("/songs/1")
+    .delete('/songs/1')
 
     .then(response => {
       expect(response.status).toEqual(200);
-      expect(response.body).toEqual(responseBody);
-    })
+      expect(response.body).toEqual(responseBody)
+    });
   });
   
   it("GET /songs should return an empty array", () => {
-    return request(app)
-    .get("/songs")
+    const responseBody = []
     
+    return request(app)
+    .get('/songs')
+
     .then(response => {
       expect(response.status).toEqual(200);
-      expect(Array.isArray(response.body)).toBe(true);
-      expect(response.body.length).toEqual(0);
+      expect(response.body).toEqual(responseBody)
     });
   });
 
